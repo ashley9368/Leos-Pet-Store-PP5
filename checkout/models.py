f0786347cc8254ebad7d7cd464e3a22db44be24e
ashley9368 <ashleygoward@icloud.com>
2025-05-15 12:01:03 +0100

    Add views as a template to change later, add bag_tools init.py

--- checkout/models.py
+++ checkout/models.py
@@ -5,7 +5,6 @@ from django.db.models import Sum
 
 from products.models import Product
 
-
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
     full_name = models.CharField(max_length=50, null=False, blank=False)
