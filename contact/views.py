e93416363a7381620ff21ca695f9175a0ddde28c
ashley9368 <ashleygoward@icloud.com>
2025-06-14 16:40:37 +0100

    build contact view with error messages

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
 
-# Create your views here.
+def contact_view(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Thanks for reaching out! We’ll get back to you soon.')
+            return redirect('contact')
+        else:
+            messages.error(request, 'There was a problem. Please check the form.')
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact.html', {'form': form})
