67c29a3b307042c5db9fa6d05b48f1c77707f85e
ashley9368 <ashleygoward@icloud.com>
2025-06-17 12:44:11 +0100

    Build wishlist model

--- leos_pet_store/settings.py
+++ leos_pet_store/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'contact',
+    'wishlist',
 
     # Other
     'crispy_forms',
--- wishlist/models.py
+++ wishlist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
 # Create your models here.
+class WishlistItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='wishlist')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # Makes sure no product can be added twice
+        unique_together = ('user', 'product')
+
+    def __str__ (self):
+        return f"{self.user.username} {self.product.name}"
