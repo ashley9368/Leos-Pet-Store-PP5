809b1b175f85438524338d3bc8471aa087f30fad
ashley9368 <ashleygoward@icloud.com>
2025-06-21 12:18:07 +0100

    add basic newsletter.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -16,6 +16,7 @@ def bag_contents(request):
         product = get_object_or_404(Product, pk=item_id)
         total += quantity * product.price
         product_count += quantity
+
         bag_items.append({
             'item_id': item_id,
             'quantity': quantity,
