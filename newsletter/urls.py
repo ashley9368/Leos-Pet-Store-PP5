65bec65e33b4cfe567eb8a75a84dc5a6e9be8b69
ashley9368 <ashleygoward@icloud.com>
2025-06-22 15:44:03 +0100

    update newsletter view, add newsletter to admin, update newsletter.html, add button to base.html

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import newsletter_signup, subscribe
+from . import views
 
 urlpatterns = [
     path('', newsletter_signup, name='newsletter'),
     path('subscribe/', subscribe, name='subscribe'),
+    path('unsubscribe/<uuid:token>/', views.unsubscribe, name='unsubscribe'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -1,14 +1,52 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from .models import NewsletterSubscriber
+from django.contrib import messages
+from django.http import HttpResponse
 
-# Create your views here.
+# Show the newsletter signup page
 def newsletter_signup(request):
-    return render(request, 'newsletter.html')
+    #If user subscribed earlier try to get the email from the session
+    email = request.session.get('subscribed_email')
+    subscriber = None
+
+    if email:
+        # If email was found in the session, find the subscriber in the database
+        subscriber = NewsletterSubscriber.objects.filter(email=email).first()
+
+    return render(request, 'newsletter.html', {'subscriber': subscriber})
 
 @require_POST
 def subscribe(request):
     email = request.POST.get('email')
     if email:
-        NewsletterSubscriber.objects.get_or_create(email=email)
+        # Check if the email exists in the database. If not create a new NewsletterSubscriber
+        subscriber, created = NewsletterSubscriber.objects.get_or_create(email=email)
+
+        if created:
+            messages.success(request, "You've been subscribed!")
+        else:
+            messages.info(request, "You're already subscribed.")
+
+        # Store the email in the session
+        request.session['subscribed_email'] = email
+    else:
+        # If email is empty or missing, show an error message
+        messages.error(request, "Please enter a valid email.")
+
+    return redirect('newsletter')
+
+@require_POST
+def unsubscribe(request, token):
+    try:
+        # Try to find the subscriber by their unique unsubscribe token
+        subscriber = NewsletterSubscriber.objects.get(unsubscribe_token=token)
+        subscriber.delete()
+        # Remove the email from the session
+        request.session.pop('subscribed_email', None)
+        messages.success(request, "You've been unsubscribed.")
+
+    except NewsletterSubscriber.DoesNotExist:
+        messages.error(request, "Invalid unsubscribe link.")
+
     return redirect('newsletter')
