0050fc267ed5fe496edf9b47e8dbe5787b49340c
ashley9368 <ashleygoward@icloud.com>
2025-06-23 16:41:49 +0100

    tidy up profiles app, re style profile.html

--- profiles/views.py
+++ profiles/views.py
@@ -8,11 +8,12 @@ from checkout.models import Order
 
 @login_required
 def profile(request):
-    """ Display the user's profile. """
+    """ Display user profile """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
+
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
