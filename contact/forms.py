b645fcfbaa44df7aead77940794b6ea9576edadf
ashley9368 <ashleygoward@icloud.com>
2025-06-23 15:41:23 +0100

    Tidy up contact page, style and center it, update forms for contact app, to apply styling to each form field

--- contact/forms.py
+++ contact/forms.py
@@ -5,3 +5,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactMessage
         fields = ['name', 'email', 'subject', 'message']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Add bootstrap styling to each form field
+        for field in self.fields.values():
+            field.widget.attrs.update({'class': 'form-control mb-3'})
