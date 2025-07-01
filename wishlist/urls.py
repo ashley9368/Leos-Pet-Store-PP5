9e6545262f2992a353ca3344675502bd84dd7f60
ashley9368 <ashleygoward@icloud.com>
2025-07-01 15:09:40 +0100

    add wishlist view, wire up urls and add basic template for wishlist

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.wishlist, name='wishlist'),
     path('toggle/<int:product_id>/',views.toggle_wishlist, name='toggle_wishlist'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse
 from .models import WishlistItem
 from products.models import Product
@@ -10,8 +10,7 @@ def toggle_wishlist(request, product_id):
     # look for product or show 404
     product = get_object_or_404(Product, id=product_id)
 
-    # See if user has this product in their wishlist
-    # If not add it
+    # See if user has this product in their wishlist, If not add it
     wishlist_item, created = WishlistItem.objects.get_or_create(
         user=request.user,
         product=product
@@ -21,3 +20,10 @@ def toggle_wishlist(request, product_id):
         wishlist_item.delete()
 
     return redirect(request.META.get('HTTP_REFERER',reverse('products')))
+
+def wishlist(request):
+    """ Show the user their wishlisted items """
+    wishlist_items = WishlistItem.objects.filter(user=request.user).select_related('product')
+    context = {'wishlist_items': wishlist_items,}
+
+    return render(request, 'wishlist.html', context)
